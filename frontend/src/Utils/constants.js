31914ad64f177a3aad30fa68a671bcf9d40df9bc
Subramani <162179474+subramani03@users.noreply.github.com>
2025-04-21 06:36:53 +0530

    Update constants.js

--- frontend/src/Utils/constants.js
+++ frontend/src/Utils/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
- location.hostname === 'localhost' ? 'http://localhost:3000/':'/api/';
+ location.hostname === 'localhost' ? 'http://localhost:3000/':'https://gateway-6epe.onrender.com/';
