f8a9833c9de92f7e087e63d27cffad3e1c6ae792
Subramani <162179474+subramani03@users.noreply.github.com>
2025-04-21 06:37:46 +0530

    Update constants.js

--- backend/Utils/constants.js
+++ backend/Utils/constants.js
@@ -1,4 +1,4 @@
 const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : '/api/';
+  process.env.NODE_ENV === 'production' ? 'http://localhost:3000/' : 'https://gateway-6epe.onrender.com/';
 console.log(process.env.NODE_ENV);
 module.exports = { BASE_URL };
